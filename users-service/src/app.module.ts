b1b5bf06b7db4178be913342464d06b3eb0dea14
Yago Inacio <yagofaran@gmail.com>
2023-12-12 18:44:36 -0300

    added exception filter

--- users-service/src/app.module.ts
+++ users-service/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { DatabaseModule } from './infra/database/database.module';
+import { GlobalExceptionFilter } from '@errors/globalException.filter';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
