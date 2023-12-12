d5ae5647b5436261cb5847380ab1f367c3a18a4f
Yago Inacio <yagofaran@gmail.com>
2023-12-12 11:01:06 -0300

    added guard to protect routes using JWT

--- wallet-service/src/app.module.ts
+++ wallet-service/src/app.module.ts
@@ -1,15 +1,27 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from './infra/database/database.module';
 import { HttpModule } from '@infra/http/http.module';
-import { APP_FILTER } from '@nestjs/core';
 import { GlobalExceptionFilter } from '@errors/globalException.filter';
+import { AuthGuard } from '@infra/http/guards/auth.guard';
 
 @Module({
-  imports: [HttpModule, DatabaseModule],
+  imports: [
+    HttpModule,
+    DatabaseModule,
+    JwtModule.register({
+      global: true,
+    }),
+  ],
   providers: [
     {
       provide: APP_FILTER,
       useClass: GlobalExceptionFilter,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
     },
   ],
 })
