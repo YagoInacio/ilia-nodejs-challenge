71370fc033523ab198158f734490d5a4ec012385
Yago Inacio <yagofaran@gmail.com>
2023-12-11 22:06:23 -0300

    added swagger lib

--- wallet-service/src/main.ts
+++ wallet-service/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Wallet Microservice')
+    .setDescription('Ilia NodeJS Challenge')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   const port = process.env.PORT || 3001;
   await app.listen(port);
