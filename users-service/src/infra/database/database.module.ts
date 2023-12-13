6191ecb59725c298044976a7f36c618c9f6ea283
Yago Inacio <yagofaran@gmail.com>
2023-12-12 21:15:50 -0300

    added prisma users repo

--- users-service/src/infra/database/database.module.ts
+++ users-service/src/infra/database/database.module.ts
@@ -1,9 +1,17 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { UsersRepository } from '@users/repositories/usersRepository';
+import { PrismaUsersRepository } from '@users/infra/database/prisma/repositories/PrismaUsersRepository';
 
 @Global()
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: UsersRepository,
+      useClass: PrismaUsersRepository,
+    },
+  ],
+  exports: [PrismaService, UsersRepository],
 })
 export class DatabaseModule {}
---
+++ users-service/src/users/infra/database/prisma/repositories/PrismaUsersRepository.ts
@@ -0,0 +1,42 @@
+import { PrismaService } from '@infra/database/prisma/prisma.service';
+import { User } from '@users/entities/user';
+import { UsersRepository } from '@users/repositories/usersRepository';
+import { PrismaUserMapper } from '../mappers/PrismaUserMapper';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PrismaUsersRepository implements UsersRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async findById(id: string): Promise<User | null> {
+    throw new Error('Method not implemented.');
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    throw new Error('Method not implemented.');
+  }
+
+  async listAll(): Promise<User[]> {
+    throw new Error('Method not implemented.');
+  }
+
+  async create(user: User): Promise<void> {
+    const mapped = PrismaUserMapper.toPrisma(user);
+
+    await this.prisma.user.create({
+      data: mapped,
+    });
+  }
+
+  async save(user: User): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  async confirm(id: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  async deactivate(id: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+}
